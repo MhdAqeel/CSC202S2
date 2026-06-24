4bbb3fc0fba338d847496253e3a19eb868c7f164
MhdAqeel <mohamedaqeel1130@gmail.com>
2026-06-24 13:04:30 +0530

    Refactor Fibonacci class to remove iterative method and improve user prompts

--- Week_02/Fibonacci.java
+++ Week_02/Fibonacci.java
@@ -1,22 +1,5 @@
 import java.util.Scanner;
 public class Fibonacci {
-    
-    static int findFib(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-
-    int a = 0;
-    int b = 1;
-
-    for (int i = 2; i <= n; i++) {
-        int temp = b;
-        b = a + b;
-        a = temp;
-    }
-
-    return b;
-    }
-
     static int recursiveFib(int n){
         if(n == 0){
             return 0; 
@@ -29,11 +12,13 @@ public class Fibonacci {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.println("Enter a positive number: ");
         int x = sc.nextInt();
-
-        System.out.println("fib: " + findFib(x));
-
-        System.out.println("recursive: " + recursiveFib(x));
+        sc.close();
+        if(x < 0){
+            System.out.println("Please enter a positive integer.");
+        } else {
+            System.out.println("Fibonacci of " + x + " is: " + recursiveFib(x));
+        }
     }
 }
