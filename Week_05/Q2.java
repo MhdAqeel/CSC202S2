e6284197ae52e8227a35bbab310357a5d78f865c
Rahuman Noor Mohamed Aqeel <mohamedaqeel1130@gmail.com>
2026-07-17 11:58:05 +0530

    Add merge function for merging two arrays
    Implement merge function to combine two sorted arrays.

---
+++ Week_05/Q2.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+public class Q2{
+	
+	public static ArrayList<Integer> merge (int[] arr1 , int[] arr2){
+		int i=0;
+		int j=arr2.length-1;
+		
+		while(i<arr1.length && j>=0){
+			if(arr1[i]<arr2[j]){
+				list.add(arr1[i]);
+				i++;
+			}
+			else{
+				list.add(arr2[j]);
+				j--;
+			}
+		}
+		
+		while
+		
+	}
+	
+	public static void main(String [] args){
+		int[] asending = {1,2,3,4,5,6};
+		int[] desending = {9,8,7,6,5,4};
+		
+		
+	}
+}
