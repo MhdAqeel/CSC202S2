e47667f984e2cef005caeeb0a6a69732c62188e0
MhdAqeel <mohamedaqeel1130@gmail.com>
2026-08-13 12:07:42 +0530

    Add StackArrayADT and StackListADT classes with basic stack operations

---
+++ Week_08/StackArrayADT.java
@@ -0,0 +1,79 @@
+public class StackArrayADT{
+	int[] stackArray;
+	int top;
+	int maxSize;
+	
+	public StackArrayADT(int maxSize){
+		stackArray = new int [maxSize];
+		this.maxSize = maxSize;
+		top = -1;
+	}
+	
+	public boolean isEmpty(){
+		return top == -1;
+	}
+
+	public boolean isFull(){
+		return top == maxSize-1;
+	}
+	
+	public void display(){
+		if(isEmpty()){
+			System.out.println("ERROR : Stack Is Empty!");
+		}
+		else{
+			System.out.println("Stack : ");
+			for(int i=top ; i>=0 ; i--){
+				System.out.print(stackArray[i] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void push(int value){
+		if(isFull()){
+			System.out.println("ERROR : Stack Over Flaw!");
+		}
+		stackArray[++top] = value;
+	}
+	
+	public int pop(){
+		if(isEmpty()){
+			System.out.println("ERROR : Stack Is Empty");
+		}
+		return stackArray[top--];
+	}
+	
+	public int peek(){
+		if(isEmpty()){
+			System.out.println("ERROR : Stack Is Empty");
+			return -1;
+		}
+		return stackArray[top];
+	}
+	
+	public static void main (String [] args){
+		StackArrayADT stack1 = new StackArrayADT(5);
+		System.out.println(stack1.isEmpty());
+		System.out.println(stack1.isFull());	
+		
+		//insert elements
+		stack1.push(1);
+		stack1.push(2);
+		stack1.push(3);
+		stack1.push(4);
+		stack1.push(5);
+		
+		//dispaly elements
+		stack1.display();
+		
+		//peek an element
+		System.out.println(stack1.peek());
+		//pop and then peek
+		System.out.println(stack1.pop());
+		System.out.println(stack1.peek());
+		
+		
+		
+	}
+}
---
+++ Week_08/StackListADT.java
@@ -0,0 +1,74 @@
+class Node {
+	String data;
+	Node next;
+	
+	public Node(String data){
+		this.data = data;
+		this.next = null;
+	}
+}
+
+class StackListADT {
+    Node top;
+	
+	public StackListADT(){
+		top = null;
+	}
+	
+	public boolean isEmpty(){
+		return top == null;
+	}
+	
+	public void push(String data){
+		Node node = new Node(data);
+		if(isEmpty()){
+			top = node;
+		}
+		else{
+			node.next=top;
+			top=node;
+		}
+	}
+	
+	public String pop(){
+		if(isEmpty()){
+			System.out.println("Stack is empty");
+			return null;
+		}
+		else{
+			String data = top.data;
+			top = top.next;
+			return data;
+		}
+		
+	}
+	
+	public String peek(){
+		if(isEmpty()){
+			System.out.println("Stack is empty");
+			return null;
+		}
+		return top.data;
+	}
+	
+	public void display(){
+		while(top!=null){
+			System.out.println(top.data);
+			top = top.next;
+		}
+	}
+	
+	
+	public static void main(String [] args){
+		StackListADT stack = new StackListADT();
+		System.out.println(stack.isEmpty());
+		stack.push("aa");
+		stack.push("bb");
+		stack.push("cc");
+		stack.push("dd");
+		stack.display();
+		
+		
+		
+	}
+}
