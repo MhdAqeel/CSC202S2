1e3f63fc76bd3ba9793e96512e4c24ac325ac49f
Aqeel <mohamedaqeel1130@gmail.com>
2026-08-20 11:12:01 +0530

    Fix queue underflow condition and enhance deQueue method in QueueArrayADT

--- Week_09/QueueArrayADT.java
+++ Week_09/QueueArrayADT.java
@@ -12,7 +12,7 @@ public class QueueArrayADT {
 	}
 	
 	public boolean isEmpty(){
-		return rear == -1;
+		return front>rear;
 	}
 	
 	public boolean isFull(){
@@ -30,13 +30,32 @@ public class QueueArrayADT {
 	
 	public String deQueue(){
 		if(isEmpty()){
-			
+			System.out.println("Error!!! Queue Under Flow.");
+			return null;
+		}
+		else{
+			String data = queueArray[front];
+			//method 01
+			front++;
+			if(front>rear){
+				front=0;
+				rear=-1;
+			}
+			return data;
+			/*
+			//method 02
+			for(int i=0; i<rear ; i++){
+				queueArray[i] = queueArray[i+1];
+			}
+			queueArray[rear--] = ""
+			return data;
+			*/
 		}
 	}
 	
 	public void display(){
 		if(isEmpty()){
-			System.out.println("Error!!! Queue Under Over Flow.");
+			System.out.println("Error!!! Queue Under Flow.");
 		}
 		else{
 			for(int i=front; i<=rear; i++){
@@ -49,6 +68,25 @@ public class QueueArrayADT {
 		QueueArrayADT queue = new QueueArrayADT(5);
 		System.out.println(queue.isEmpty());
 		System.out.println(queue.isFull());
-		
+		queue.enQueue("a1");
+		queue.enQueue("b2");
+		queue.enQueue("c3");
+		queue.display();
+		System.out.println("--------------------");
+		queue.deQueue();
+		queue.deQueue();
+		queue.display();
+		System.out.println("--------------------");
+		queue.deQueue();
+		queue.display();
+		System.out.println("--------------------");
+		queue.enQueue("d1");
+		queue.enQueue("e2");
+		queue.enQueue("f3");
+		queue.enQueue("a2");
+		queue.enQueue("b3");
+		queue.enQueue("c4");
+		queue.display();
+		System.out.println("--------------------");
 	}
 }
